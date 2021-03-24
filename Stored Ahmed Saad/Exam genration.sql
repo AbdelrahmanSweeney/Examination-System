e7bb3636196590a92b3505585a566d9d00680855
mostafagwely2 <48653322+mostafagwely2@users.noreply.github.com>
2021-03-24 22:44:15 +0200

    Update Exam genration.sql

--- Stored Ahmed Saad/Exam genration.sql
+++ Stored Ahmed Saad/Exam genration.sql
@@ -1,8 +1,9 @@
-Alter PROC Exam_Random_generation  @cr_name VARCHAR(20), @no_TF INT, @no_ML INT, @exam_time int, @exam_gread int
+alter PROC Exam_Random_generation  @cr_name VARCHAR(20), @no_TF INT, @no_MC INT, @exam_time int, @exam_grade int
 AS
 begin
+/*insert exam into exam table*/
 	INSERT INTO Exam(exam_grade, exam_time, cr_id)
-	VALUES     (@exam_gread, @exam_time,
+	VALUES     (@exam_grade, @exam_time,
 			   (SELECT cr_id FROM course WHERE cr_name = @cr_name))
 
 DECLARE @ex_id INT
@@ -15,18 +16,18 @@ INSERT INTO Exam_Question    --Generate true & false Question
 	WHERE C.cr_name = @cr_name AND Q.qType IN('YN','TF')
 	ORDER BY NEWID()	
 INSERT INTO Exam_Question	--Generate true & false Question
-	SELECT TOP(@no_ML) @ex_id, Q.ques_id
+	SELECT TOP(@no_MC) @ex_id, Q.ques_id
 	FROM Questions Q JOIN question_course QC ON Q.ques_id = QC.ques_id
 					 JOIN course C ON C.cr_id =QC.cr_id
-	WHERE c.cr_name =@cr_name AND Q.qType IN('ML')
+	WHERE c.cr_name =@cr_name AND Q.qType IN('MC')
 	ORDER BY NEWID()
 
-SELECT  Q.question, mcq.choices  --Return Exam Question
-	FROM Questions Q JOIN Choices mcq ON Q.ques_id=mcq.ques_id
+SELECT  Q.question,Q.ques_id, Q.qType, mcq.a, mcq.b, mcq.c, mcq.d  --Return Exam Question
+	FROM Questions Q left JOIN Choices mcq ON Q.ques_id=mcq.ques_id
 	JOIN Exam_Question EX ON Q.ques_id = EX.ques_id
 	WHERE EX.exam_id = @ex_id
 
 end
 
-Execute Exam_Random_generation @cr_name='Programming', @no_TF=2, @no_ML=1 ,@exam_time=60 ,@exam_gread=20
+Execute Exam_Random_generation @cr_name='C#', @no_TF=5, @no_MC=5 ,@exam_time=60 ,@exam_grade=20
 
